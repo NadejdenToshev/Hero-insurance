e201c5cb96337864a004a0adc616e424c0566bab
nadejdentoshev <nadejdentoshev@gmail.com>
2024-09-30 14:01:33 +0300

    remove hero-insurance/ in urlpatterns=path

--- HeroInsurance/HeroInsurance/urls.py
+++ HeroInsurance/HeroInsurance/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("insurance/", include("insurance.urls")),
+    path("", include("insurance.urls")),
     path('admin/', admin.site.urls),
 ]
